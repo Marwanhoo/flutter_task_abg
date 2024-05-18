b63bec1d7a86625519dfbe0c090471cecb6dda28
Marwanhoo <muh.marwanhoo@gmail.com>
2024-05-19 02:51:38 +0300

    handle button logout if user null

--- lib/feature/home/view/home_screen.dart
+++ lib/feature/home/view/home_screen.dart
@@ -4,7 +4,8 @@ import 'package:flutter_task_abg/feature/auth/controller/cubit/auth_cubit.dart';
 import 'package:flutter_task_abg/feature/auth/view/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key,  this.username});
+  const HomeScreen({super.key, this.username});
+
   final String? username;
 
   @override
@@ -13,31 +14,39 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Column(
         children: [
-           Text("Hello ${username ?? "Guest"}"),
-          ElevatedButton(onPressed: (){
-            //BlocProvider.of<AuthCubit>(context).logout();
-            showDialog(context: context, builder: (context) {
-              return AlertDialog(
-                title: Text("Logout"),
-                content:
-                const Text("Are you sure you want to logout?"),
-                actions: [
-                  TextButton(
-                    onPressed: () async {
-                      BlocProvider.of<AuthCubit>(context).logout();
-                      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (_)=> const LoginScreen()), (route) => false);
-                    },
-                    child: const Text("Ok"),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Text("Cancel")),
-                ],
+          Text("Hello ${username ?? "Guest"}"),
+          if(username != null) ElevatedButton(
+            onPressed: () {
+              //BlocProvider.of<AuthCubit>(context).logout();
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Logout"),
+                    content: const Text("Are you sure you want to logout?"),
+                    actions: [
+                      TextButton(
+                        onPressed: () async {
+                          BlocProvider.of<AuthCubit>(context).logout();
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (_) => const LoginScreen()),
+                              (route) => false);
+                        },
+                        child: const Text("Ok"),
+                      ),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("Cancel")),
+                    ],
+                  );
+                },
               );
-            },);
-          }, child: const Text("Logout"),),
+            },
+            child: const Text("Logout"),
+          ),
         ],
       ),
     );
